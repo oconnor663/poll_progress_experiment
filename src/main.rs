386513bbd60af6d28b46393aff6ac2c8aba12c98
Jack O'Connor <oconnor663@gmail.com>
2026-07-12 20:48:07 -0700

    the lazy merge version

--- src/main.rs
+++ src/main.rs
@@ -25,18 +25,25 @@ trait AsyncIterator {
         Fut: Future,
     {
         Then {
-            iter: self,
+            iter: self.fuse(),
+            next_item_wanted: false,
             f,
             fut: None,
+            item: None,
         }
     }
 
-    fn map<F, T>(self, f: F) -> Map<Self, F>
+    fn map<F, T>(self, f: F) -> Map<Self, F, T>
     where
         Self: Sized,
         F: FnMut(Self::Item) -> T,
     {
-        Map { iter: self, f }
+        Map {
+            iter: self.fuse(),
+            next_item_wanted: false,
+            f,
+            item: None,
+        }
     }
 
     fn merge<Other>(self, other: Other) -> Merge<Self, Other>
@@ -47,9 +54,6 @@ trait AsyncIterator {
         Merge {
             left: self.fuse(),
             right: other.fuse(),
-            poll_next_never_called: true,
-            left_is_current: true,
-            buffered: None,
         }
     }
 
@@ -72,16 +76,6 @@ enum PollNext<Item> {
     Item(Item),
     Pending,
     Done,
-}
-
-impl<Item> PollNext<Item> {
-    fn map<T>(self, f: impl FnOnce(Item) -> T) -> PollNext<T> {
-        match self {
-            PollNext::Item(item) => PollNext::Item(f(item)),
-            PollNext::Pending => PollNext::Pending,
-            PollNext::Done => PollNext::Done,
-        }
-    }
 }
 
 fn iter<I>(iter: I) -> Iter<I::IntoIter>
@@ -163,14 +157,16 @@ where
 
 pin_project! {
     #[must_use]
-    struct Map<Iter, F> {
+    struct Map<Iter, F, T> {
         #[pin]
-        iter: Iter,
+        iter: Fuse<Iter>,
+        next_item_wanted: bool,
         f: F,
+        item: Option<T>,
     }
 }
 
-impl<Iter, F, T> AsyncIterator for Map<Iter, F>
+impl<Iter, F, T> AsyncIterator for Map<Iter, F, T>
 where
     Iter: AsyncIterator,
     F: FnMut(Iter::Item) -> T,
@@ -178,12 +174,38 @@ where
     type Item = T;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> PollNext<Self::Item> {
-        let this = self.project();
-        this.iter.poll_next(cx).map(this.f)
+        let mut this = self.project();
+        if let Some(item) = this.item.take() {
+            return PollNext::Item(item);
+        }
+        match this.iter.as_mut().poll_next(cx) {
+            PollNext::Item(item) => {
+                *this.next_item_wanted = false;
+                PollNext::Item((this.f)(item))
+            }
+            PollNext::Pending => {
+                *this.next_item_wanted = true;
+                PollNext::Pending
+            }
+            PollNext::Done => PollNext::Done,
+        }
     }
 
     fn poll_progress(self: Pin<&mut Self>, cx: &mut Context) -> Poll<()> {
-        self.project().iter.poll_progress(cx)
+        let mut this = self.project();
+        if this.item.is_none() && *this.next_item_wanted {
+            match this.iter.as_mut().poll_next(cx) {
+                PollNext::Item(item) => {
+                    *this.item = Some((this.f)(item));
+                    *this.next_item_wanted = false;
+                    this.iter.poll_progress(cx)
+                }
+                PollNext::Pending => Poll::Pending,
+                PollNext::Done => Poll::Ready(()),
+            }
+        } else {
+            this.iter.poll_progress(cx)
+        }
     }
 }
 
@@ -196,10 +218,12 @@ pin_project! {
         Fut: Future,
     {
         #[pin]
-        iter: Iter,
+        iter: Fuse<Iter>,
+        next_item_wanted: bool,
         f: F,
         #[pin]
         fut: Option<Fut>,
+        item: Option<Fut::Output>,
     }
 }
 
@@ -213,18 +237,23 @@ where
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> PollNext<Fut::Output> {
         let mut this = self.project();
-
+        if let Some(item) = this.item.take() {
+            return PollNext::Item(item);
+        }
         if this.fut.is_none() {
             match this.iter.as_mut().poll_next(cx) {
                 PollNext::Item(item) => {
+                    *this.next_item_wanted = false;
                     let fut = (this.f)(item);
                     this.fut.set(Some(fut));
                 }
+                PollNext::Pending => {
+                    *this.next_item_wanted = true;
+                    return PollNext::Pending;
+                }
                 PollNext::Done => return PollNext::Done,
-                PollNext::Pending => return PollNext::Pending,
             }
         }
-
         let fut = this.fut.as_mut().as_pin_mut().expect("populated above");
         match fut.poll(cx) {
             Poll::Ready(output) => {
@@ -239,8 +268,35 @@ where
     }
 
     fn poll_progress(self: Pin<&mut Self>, cx: &mut Context) -> Poll<()> {
-        assert!(self.fut.is_none());
-        self.project().iter.poll_progress(cx)
+        let mut this = self.project();
+        if this.item.is_some() {
+            return this.iter.poll_progress(cx);
+        }
+        if this.fut.is_none() && *this.next_item_wanted {
+            match this.iter.as_mut().poll_next(cx) {
+                PollNext::Item(item) => {
+                    *this.next_item_wanted = false;
+                    this.fut.set(Some((this.f)(item)));
+                }
+                PollNext::Pending => return Poll::Pending,
+                PollNext::Done => return Poll::Ready(()),
+            }
+        }
+        if let Some(fut) = this.fut.as_mut().as_pin_mut() {
+            match fut.poll(cx) {
+                Poll::Ready(item) => {
+                    this.fut.set(None);
+                    *this.item = Some(item);
+                    this.iter.poll_progress(cx)
+                }
+                Poll::Pending => {
+                    _ = this.iter.poll_progress(cx);
+                    Poll::Pending
+                }
+            }
+        } else {
+            this.iter.poll_progress(cx)
+        }
     }
 }
 
@@ -253,40 +309,8 @@ fn any_pending(polls: impl IntoIterator<Item = Poll<()>>) -> Poll<()> {
     Poll::Ready(())
 }
 
-fn update_pending(ret: &mut Poll<()>, poll: Poll<()>) {
-    if poll.is_pending() {
-        *ret = Poll::Pending;
-    }
-}
-
-enum EitherPin<'a, S1, S2> {
-    Left(Pin<&'a mut S1>),
-    Right(Pin<&'a mut S2>),
-}
-
-impl<'a, S1, S2> EitherPin<'a, S1, S2>
-where
-    S1: AsyncIterator,
-    S2: AsyncIterator<Item = S1::Item>,
-{
-    fn poll_next(self, cx: &mut Context) -> PollNext<S1::Item> {
-        match self {
-            EitherPin::Left(s1) => s1.poll_next(cx),
-            EitherPin::Right(s2) => s2.poll_next(cx),
-        }
-    }
-
-    fn poll_progress(self, cx: &mut Context) -> Poll<()> {
-        match self {
-            EitherPin::Left(s1) => s1.poll_progress(cx),
-            EitherPin::Right(s2) => s2.poll_progress(cx),
-        }
-    }
-}
-
 pin_project! {
     #[must_use]
-    #[project = MergeProj]
     pub struct Merge<Left, Right>
     where
         Left: AsyncIterator,
@@ -296,37 +320,6 @@ pin_project! {
         left: Fuse<Left>,
         #[pin]
         right: Fuse<Right>,
-        poll_next_never_called: bool,
-        left_is_current: bool,
-        buffered: Option<Left::Item>,
-    }
-}
-
-impl<'p, Left, Right> MergeProj<'p, Left, Right>
-where
-    Left: AsyncIterator,
-    Right: AsyncIterator<Item = Left::Item>,
-{
-    fn current_iter(
-        self: &mut MergeProj<'p, Left, Right>,
-    ) -> EitherPin<'_, Fuse<Left>, Fuse<Right>> {
-        if *self.left_is_current {
-            EitherPin::Left(self.left.as_mut())
-        } else {
-            EitherPin::Right(self.right.as_mut())
-        }
-    }
-
-    fn other_iter(self: &mut MergeProj<'p, Left, Right>) -> EitherPin<'_, Fuse<Left>, Fuse<Right>> {
-        if *self.left_is_current {
-            EitherPin::Right(self.right.as_mut())
-        } else {
-            EitherPin::Left(self.left.as_mut())
-        }
-    }
-
-    fn swap_current_iter(self: &mut MergeProj<'p, Left, Right>) {
-        *self.left_is_current = !*self.left_is_current;
     }
 }
 
@@ -339,17 +332,12 @@ where
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> PollNext<Self::Item> {
         let mut this = self.project();
-        *this.poll_next_never_called = false;
-        if let Some(item) = this.buffered.take() {
-            // If there's a buffered item, `poll_progress` got it from the current iterator.
-            this.swap_current_iter();
+        // XXX: This implementation is simple but unfair. If the left is always ready, the right
+        // will never get polled. Not a problem in this particular example.
+        if let PollNext::Item(item) = this.left.as_mut().poll_next(cx) {
             return PollNext::Item(item);
         }
-        if let PollNext::Item(item) = this.current_iter().poll_next(cx) {
-            this.swap_current_iter();
-            return PollNext::Item(item);
-        }
-        if let PollNext::Item(item) = this.other_iter().poll_next(cx) {
+        if let PollNext::Item(item) = this.right.as_mut().poll_next(cx) {
             return PollNext::Item(item);
         }
         if this.left.is_done() && this.right.is_done() {
@@ -360,27 +348,11 @@ where
     }
 
     fn poll_progress(self: Pin<&mut Self>, cx: &mut Context) -> Poll<()> {
-        let mut this = self.project();
-        if *this.poll_next_never_called {
-            let poll1 = this.left.as_mut().poll_progress(cx);
-            let poll2 = this.right.as_mut().poll_progress(cx);
-            return any_pending([poll1, poll2]);
-        }
-        let mut ret = Poll::Ready(());
-        if this.buffered.is_none() {
-            match this.current_iter().poll_next(cx) {
-                PollNext::Item(item) => *this.buffered = Some(item),
-                PollNext::Done => {}
-                PollNext::Pending => ret = Poll::Pending,
-            }
-        }
-        if this.buffered.is_some() {
-            let poll = this.current_iter().poll_progress(cx);
-            update_pending(&mut ret, poll);
-        }
-        let poll = this.other_iter().poll_progress(cx);
-        update_pending(&mut ret, poll);
-        ret
+        let this = self.project();
+        // XXX: This implementation is lazy. It makes its callees much more complicated.
+        let poll1 = this.left.poll_progress(cx);
+        let poll2 = this.right.poll_progress(cx);
+        any_pending([poll1, poll2])
     }
 }
 
